4fb8984d7a58c8adb3b24f8c9353dc733902be75
Muhammad Ferdy <ferdyzecter@gmail.com>
2021-01-16 10:18:54 +0700

    Finel

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,31 @@ namespace ClientChatApp_054_MuhammadFerdyYosa
 {
     class Program
     {
-        static void Main(string[] args)
+        public class ClientCallback : ServiceReference1.IServiceCallbackCallback
         {
+            public void pesanKirim(string user, string pesan)
+            {
+                Console.WriteLine("{0}: {1}", user, pesan);
+            }
+            static void Main(string[] args)
+        {
+                InstanceContext context = new InstanceContext(new ClientCallback());
+                ServiceReference1.ServiceCallbackClient server = new ServiceReference1.ServiceCallbackClient(context);
 
-        }
+                Console.WriteLine("Enter Username");
+                string nama = Console.ReadLine();
+                server.gabung(nama);
+
+                Console.WriteLine("Kirim Pesan");
+                string pesan = Console.ReadLine();
+
+                while (true)
+                {
+                    if (!string.IsNullOrEmpty(pesan))
+                        server.kirimPesan(pesan);
+                    Console.WriteLine("Kirim Pesan");
+                    pesan = Console.ReadLine();
+                }
+            }
     }
 }
