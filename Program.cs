14f068d3d39daa8f825f3eee21fda1a1b3b94780
Muhammad Ferdy <ferdyzecter@gmail.com>
2021-01-16 10:09:04 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientChatApp_054_MuhammadFerdyYosa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
